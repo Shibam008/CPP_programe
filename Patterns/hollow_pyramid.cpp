743d6561baf722382377bf27da54b68c10a9af2a
Shibam Sadhukhan <sadhukhan799@gmail.com>
2023-09-09 13:30:43 +0530

    pattern9 edited

--- Patterns/hollow_pyramid.cpp
+++ Patterns/hollow_pyramid.cpp
@@ -5,7 +5,7 @@ int main (){
     cout << "Enter number of rows : ";
     cin >> n ;
 
-    //for loop er modhye conditional part er range tai hochhe total value for that loop.
+    //ekhane FULL_PYRAMID er program likhe if-else use krte hbe .
 
     for(int row=0; row<n; row++){ //total row = n
         //for space printing
@@ -13,7 +13,7 @@ int main (){
             cout << " " ; //one space 
         }
         for(int col=0; col<row+1; col++){   //here total column = row+1
-            if(col==0 || col==(row+1)-1){    //for 0 based calculation total col 'X' hole [1st = 0 & last = X-1]
+            if(col==0 || col==(row+1)-1){    
                 cout << "* ";
             } 
             else{
