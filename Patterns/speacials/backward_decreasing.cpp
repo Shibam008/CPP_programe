22a3034efca575216ef78538bca0d47b2b0a3784
Shibam Sadhukhan <sadhukhan799@gmail.com>
2023-11-28 06:17:30 +0530

    pattern added

---
+++ Patterns/speacials/backward_decreasing.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter number of rows : ";
+    cin>>n;
+    for(int row=0; row<n; row++){
+        for(int col = n-row; col>=1; col--){
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
+}
