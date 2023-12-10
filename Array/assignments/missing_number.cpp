20f799f647a1469f7f4a887c01b695b51ba64a3e
Shibam Sadhukhan <sadhukhan799@gmail.com>
2023-12-10 20:01:11 +0530

    modified

--- Array/assignments/missing_number.cpp
+++ Array/assignments/missing_number.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+
+//missing element/number er ei Question tar best solution  [Binary Search]  use kore korte hoy.
+
+// **checkout this problem on "searching & sorting" folder
+
 int missingnum(vector<int> &nums){
     int ans=0;
 //XOR of all values/elements of array.
@@ -14,17 +19,8 @@ int missingnum(vector<int> &nums){
     return ans;
 }
 int main(){
-    vector<int> nums;
-    nums.push_back(9);
-    nums.push_back(0);
-    nums.push_back(2);
-    nums.push_back(1);
-    nums.push_back(3);
-    nums.push_back(6);
-    nums.push_back(5);
-    nums.push_back(4);
-    nums.push_back(7);
-
+    vector<int> nums = {9,0,2,1,3,6,5,4,7};
     int ans = missingnum(nums);
     cout<<"Missing number is : "<<ans<<endl;
 }
+//time complexity - O(n).
