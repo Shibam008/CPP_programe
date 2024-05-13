fd1444bc79384766477732d56dd01e1fd5891971
Shibam Sadhukhan <sadhukhan799@gmail.com>
2024-05-13 13:37:16 +0530

    recursion

--- Recursion/last_occ_of_char.cpp
+++ Recursion/last_occ_of_char.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-void checkLastOccur(string str, char target, int index, int&ans){
+void checkLastOccur_L2R(string str, char target, int index, int&ans){
     // base case
     if(index >= str.length()) {
         return;
@@ -10,17 +10,34 @@ void checkLastOccur(string str, char target, int index, int&ans){
         ans = index;
     }
     // recursive call
-    checkLastOccur(str, target, index+1, ans);
+    checkLastOccur_L2R(str, target, index+1, ans);
+}
+
+void checkLastOccur_R2L(string str, char target, int index, int&ans){
+    // base case
+    if(index <= 0) {
+        return;
+    }
+    // handle one case
+    if(str[index] == target) {
+        ans = index;
+        return;
+    }
+    // recursive call
+    checkLastOccur_R2L(str, target, index-1, ans);
 }
 
 int main() {
     string str = "abbcddefgdh";
+    int size = str.length();
+
     char target;
     cout<<"enter char : ";
     cin>>target;
 
     int ans = -1;
-    checkLastOccur(str, target, 0, ans);
+    checkLastOccur_L2R(str, target, 0, ans);
+    //checkLastOccur_R2L(str, target, size-1, ans);
     cout<<ans<<endl;
     
 }
