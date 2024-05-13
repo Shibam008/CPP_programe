a0a9224bd5a3d5102793a4fcbac10ad7a9cfde95
Shibam Sadhukhan <sadhukhan799@gmail.com>
2024-05-13 23:05:42 +0530

    stack

--- Stack/string_Rev_Using_Stack.cpp
+++ Stack/string_Rev_Using_Stack.cpp
@@ -4,8 +4,7 @@ int main() {
     string str = "rock";
     stack<char> st;
 
-    for(int i=0; i<str.length(); i++) {
-        char ch = str[i];
+    for(auto ch : str) {
         st.push(ch);
     }
 
