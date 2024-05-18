31e3e44724514856a187c219c624b6d735b82cf3
Shibam Sadhukhan <sadhukhan799@gmail.com>
2024-05-18 18:49:21 +0530

    Stack

---
+++ Stack/insert_elem_in_sorted_stack.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+void insertAtSortedStack(stack<int> &st, int element) {
+    // base case
+    if(element > st.top()) {
+        st.push(element);
+        return;
+    }
+    // handle one case
+    int temp = st.top();
+    st.pop();
+    // recursive call
+    insertAtSortedStack(st, element);
+    //back track
+    st.push(temp);
+}
+int main() {
+    stack<int> st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+
+    int elem;
+    cout<<"Enter element : ";
+    cin>>elem; 
+
+    insertAtSortedStack(st, elem);
+
+    cout<<"Stack : ";
+    while(!st.empty()) {
+        cout<<st.top()<<" ";
+        st.pop();
+    }cout<<endl;
+
+    
+   return 0;
+}
