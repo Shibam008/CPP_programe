d4b410a8bbcb029abfc55fdce11b938910da1cf5
Shibam Sadhukhan <sadhukhan799@gmail.com>
2024-08-19 13:16:04 +0530

    find kth smallest and largest element

--- Heap/kth_smallest_elem.cpp
+++ Heap/kth_smallest_elem.cpp
@@ -1,9 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int findKthSmallest(int *arr, int n, int k) 
+int findKthSmallest(int * arr, int n, int k) 
 {
-    priority_queue<int> pq;
+    priority_queue<int> pq;  //! Max heap
 
     // process k elements first
     for(int i=0; i<k; i++) {
@@ -24,11 +24,35 @@ int findKthSmallest(int *arr, int n, int k)
     return ans;
 }
 
+int findKthLargest(int * arr, int n, int k) 
+{
+    priority_queue<int, vector<int>, greater<int> > pq;  //! Min heap
+
+    // process k elements first
+    for(int i=0; i<k; i++) {
+        int elem = arr[i];
+        pq.push(elem);
+    }
+
+    //compare remaining element and insert into heap if any elem is greater than root of heap
+    for(int i=k; i<n; i++) {
+        int elem = arr[i];
+        if(elem > pq.top()) {
+            pq.pop();
+            pq.push(elem);
+        }
+    }
+
+    int ans = pq.top();
+    return ans;
+}
+
 int main() {
     int arr[] = {1, 9, 6, 3, 4, 8};
     int size = 6;
     int k = 3;
 
-    int ans = findKthSmallest(arr, size, k);
-    cout << " k-th smallest elements : " << ans << endl;
+    //int ans = findKthSmallest(arr, size, k);
+    int ans = findKthLargest(arr, size, k);
+    cout << " k-th largest elements : " << ans << endl;
 }
